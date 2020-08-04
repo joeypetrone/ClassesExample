e5a79e45674d569a7fa01399186a56bda725461c
Joey Petrone <joey.petrone@gmail.com>
2020-08-03 19:40:50 -0500

    Add project files.

---
+++ ClassesExample/Animals/Chicken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample.Animals
+{
+    class Chicken
+    {
+        //property
+        public string Name { get; set; }
+
+
+        //default constructor
+        //public Chicken() { }
+
+        //constructor
+        public Chicken()
+        {
+            Console.WriteLine("Chicken got instantiated...");
+        }
+
+        //method (signature = accessibility modifier -> return type -> Name -> Parameters
+        public void Peck(string typeOfFood)
+        {
+            Console.WriteLine($"{Name} pecks hungrily at the {typeOfFood}.");
+        }
+    }
+}
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassesExample.Animals;
+using System;
+
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken = new Chicken();
+
+            chicken.Name = "Liza";
+
+            chicken.Peck("Corn");
+        }
+    }
+}
