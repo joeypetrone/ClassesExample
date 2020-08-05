ff680d777c8f30f87b586176d4a4677a5638626e
Joey Petrone <joey.petrone@gmail.com>
2020-08-04 21:00:26 -0500

    Cat class is working

---
+++ ClassesExample/Animals/Cat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample.Animals
+{
+    class Cat
+    {
+        public CatColor Color {get;set;}
+
+        public bool IsFeral {get;set;}
+
+        public Chicken BestFriend {get;set;}
+
+        public void Dye(CatColor color)
+        {
+            Color = color;
+        }
+
+        public void HaveASnack()
+        {
+            Console.WriteLine($"The {Color} cat took a bite of {BestFriend.Name}. Ooops!");
+            BestFriend.Peck($"The {Color} cat.");
+        }
+
+
+    }
+
+    enum CatColor
+    {
+        NotSetColor,
+        Orange,
+        Grey,
+        Black,
+        Calico,
+        Tabby,
+        White
+    }
+}
